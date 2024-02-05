c87b907ac71b8fd367ff8a6acbedc57cf4e8e7af
Baelrin <Baelrin-Revel@yandex.ru>
2024-02-05 07:12:08 +0300

    Basic structure

--- master.py
+++ master.py
@@ -1,1 +1,20 @@
+import sqlite3
+import datetime
 
+conn = sqlite3.connect('expenses.db')
+cur = conn.cursor()
+
+while True:
+    print("Select an optin:")
+    print("1. Enter a new expense")
+    print("2. View expenses summary")
+
+    choice = int(input())
+    
+    if choice == 1:
+        date = input("Enter the date of the expense(YYYY-MM-DD): ")
+        description = input("Enter the description of the expense: ")
+    elif choice == 2:
+        pass 
+    else:
+        exit()
